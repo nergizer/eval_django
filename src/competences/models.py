b61c00eb478a92b1ea5ae0943e0b62eaa4d0218f
Martial Bret <martial.bret@fms-ea.com>
2024-11-14 12:05:10 +0100

    Reformatage du fichier pour ne plus avoir de warnings / PEP8

--- src/competences/models.py
+++ src/competences/models.py
@@ -10,7 +10,6 @@ from django.db.models import Model, QuerySet
 class Competence(Model):
     nom = models.CharField(max_length=256)
 
-
     def __str__(self):
         return self.nom
 
@@ -20,27 +19,26 @@ class Competence(Model):
 
 class UserProfile(Model):
     user = models.ForeignKey(to=User, primary_key=True, on_delete=models.CASCADE)
-    competences = models.ManyToManyField(to=Competence,default=[])
+    competences = models.ManyToManyField(to=Competence, default=[])
 
-    def get_requetes(self,competences : set[Competence]) -> QuerySet[Requete]:
-        return Requete.objects.filter(owner=self,competence__in=competences)
+    def get_requetes(self, competences: set[Competence]) -> QuerySet[Requete]:
+        return Requete.objects.filter(owner=self, competence__in=competences)
 
     @classmethod
-    def get_for_user(cls,user : User) -> UserProfile:
+    def get_for_user(cls, user: User) -> UserProfile:
         try:
             return cls.objects.get(pk=user.id)
         except UserProfile.DoesNotExist:
             return cls.objects.create(user=user)
 
+
 class Requete(Model):
     description = models.CharField(max_length=256)
     date = models.DateField()
-    competence = models.ForeignKey(to=Competence,on_delete=models.CASCADE)
-    owner = models.ForeignKey(to=UserProfile,on_delete=models.CASCADE)
-    assigned = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE,related_name="assigned", blank=True, null=True )
-
-
+    competence = models.ForeignKey(to=Competence, on_delete=models.CASCADE)
+    owner = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE)
+    assigned = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name="assigned", blank=True,
+                                 null=True)
 
     def __str__(self):
         return self.description + ": " + str(self.competence) + ", le " + str(self.date)
-
