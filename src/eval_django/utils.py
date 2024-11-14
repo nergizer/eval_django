4beeb7da0efd063ca336f21b330d71a0c8b2f004
Mathis <mathis.aupetit@fms-ea.com>
2024-11-14 08:52:41 +0100

    pytest cov

--- src/eval_django/utils.py
+++ src/eval_django/utils.py
@@ -6,6 +6,7 @@ from typing_extensions import Callable, Any
 
 
 def try_or[O](try_block: Callable[[],O],else_return : O) -> O:
+    """Try to call lambda try_block, or return else_return in case of an excepetion"""
     try:
         return try_block()
     except Exception:
@@ -13,6 +14,7 @@ def try_or[O](try_block: Callable[[],O],else_return : O) -> O:
         return else_return
 
 def spath(name : str,view : Callable[...,HttpResponseBase]) -> URLPattern:
+    """Simple parameterless path util"""
     assert '/' not in name
     assert '<' not in name
     return path(name,view,name=name)
