eacc5d64e02382ffb70f3c0e9e06d600e94c429e
Mathis <mathis.aupetit@fms-ea.com>
2024-11-14 09:08:25 +0100

    doctests

--- src/eval_django/utils.py
+++ src/eval_django/utils.py
@@ -6,10 +6,19 @@ from typing_extensions import Callable, Any
 
 
 def try_or[O](try_block: Callable[[],O],else_return : O) -> O:
-    """Try to call lambda try_block, or return else_return in case of an excepetion"""
+    """Try to call lambda try_block, or return else_return in case of an excepetion
+    The exception message is printed to stderr
+    >>> try_or(lambda : 1/0,0)
+    0
+    >>> try_or(lambda : 1/2,-1)
+    0.5
+    >>> ex = lambda: ["a"][2]
+    >>> try_or(ex,"Error")
+    'Error'
+    """
     try:
         return try_block()
-    except Exception:
+    except BaseException:
         traceback.print_exc()
         return else_return
 
